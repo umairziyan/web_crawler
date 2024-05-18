29939b92607034dc3637b6d012fc7785a0db6c4c
umairziyan <umairziyan@protonmail.com>
2024-05-18 20:57:52 +0100

    Added first function and test files.

---
+++ crawl.js
@@ -0,0 +1,11 @@
+function normalizeURL(url) {
+  // Function to normalize any url by using the url object.
+  const urlObj = new URL(url);
+  let fullPath = `${urlObj.host}${urlObj.pathname}`;
+  if (fullPath.slice(-1) === "/") {
+    fullPath = fullPath.slice(0, -1);
+  }
+  return fullPath;
+}
+
+export { normalizeURL };
---
+++ crawl.test.js
@@ -0,0 +1,37 @@
+import { normalizeURL } from "./crawl.js";
+import { test, expect } from "@jest/globals";
+
+test("normalizeURL protocol", () => {
+  const input = "https://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL end_slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL http", () => {
+  const input = "http://blog.boot.dev/path";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL http with slash", () => {
+  const input = "https://blog.boot.dev/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("normalizeURL random caps", () => {
+  const input = "https://bloG.boot.dEv/path/";
+  const actual = normalizeURL(input);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
